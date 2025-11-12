d64108db5b1c852c732cf2bb813f58df8bd87e73
Davi <baptistadossantosdavi@gmail.com>
2025-11-12 18:21:27 -0300

    serviço de usuario

--- src/modules/users/user.repo.sequelize.js
+++ src/modules/users/user.repo.sequelize.js
@@ -1,63 +1,22 @@
 import { User } from '../../models/index.js';
 
-export function makeUserRepoSequelize() {
-return {
-async create({ name, email, passwordHash }) {
-const user = await User.create({ name, email, passwordHash });
-return user.toJSON();
-},
+export const makeUserRepoSequelize = () => {
+    const create = async ({ name, email, passwordHash, nickname }) => {
+        const user = await User.create({ name, email, passwordHash, nickname });
+        return user.toJSON();
+    }
 
+    const findByEmail = async (email) => {
+        const user = await User.findOne({ where: { email } });
+        return user ? user.toJSON() : null;
+    }
 
-async findByEmail(email) {
-const user = await User.findOne({ where: { email } });
-return user ? user.toJSON() : null;
-},
+    const findById = async (id) => {
+        const user = await User.findByPk(id, {
+            attributes: { exclude: ['passwordHash'] } // Não retornar senha
+        });
+        return user ? user.toJSON() : null;
+    }
 
-
-async findById(id) {
-const user = await User.findByPk(id, {
-attributes: { exclude: ['passwordHash'] } // Não retornar senha
-});
-return user ? user.toJSON() : null;},
-
-
-async list({ page = 1, limit = 10 }) {
-const offset = (page - 1) * limit;
-
-
-const { count, rows } = await User.findAndCountAll({
-attributes: { exclude: ['passwordHash'] },
-limit,
-offset,
-order: [['createdAt', 'DESC']]
-});
-
-
-return {
-items: rows.map(u => u.toJSON()),
-page,
-limit,
-total: count,
-totalPages: Math.ceil(count / limit)
-};
-},
-
-
-async update(id, data) {
-const user = await User.findByPk(id);
-if (!user) return null;
-await user.update(data);
-return user.toJSON();
-},
-
-
-async delete(id) {
-const user = await User.findByPk(id);
-if (!user) return false;
-
-
-await user.destroy();
-return true;
-}
-};
+    return { create, findById, findByEmail }
 }
