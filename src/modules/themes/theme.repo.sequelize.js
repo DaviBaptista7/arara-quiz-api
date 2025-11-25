25adaf1f1f94f8d9b352a4f8112d1ffa0ec3ed8d
Davi <baptistadossantosdavi@gmail.com>
2025-11-24 21:24:11 -0300

    Implementado metodo de busca por id

--- src/modules/themes/theme.repo.sequelize.js
+++ src/modules/themes/theme.repo.sequelize.js
@@ -1,11 +1,18 @@
 import { Theme } from "../../models/Theme.js"
 
 export const makeThemeRepoSequelize = () => {
+    const findById = async ({ id }) => {
+
+        const theme = await Theme.findByPk(id)
+
+        return theme ? theme.toJSON() : null
+    }
+
     const findAll = async () => {
         const themes = await Theme.findAll()
 
         return themes.map(theme => theme.toJSON())
     }
 
-    return { findAll }
+    return { findAll, findById }
 }
